180132751848e29992f1f73f3692e2db22680dd1
Shrimali Om <129108275+Shrimali-om1@users.noreply.github.com>
2024-02-03 14:07:40 +0000

    last shit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId =144
+let accountEmail = "shrimalik471@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahemadabad"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
